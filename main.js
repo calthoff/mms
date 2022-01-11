b0a3c7d5d9ad6e755f79978c171956d8504db543
Cory Althoff <cory@theselftaughtprogrammer.io>
2022-01-10 23:58:43 -0800

    first

---
+++ main.js
@@ -0,0 +1,36 @@
+const Vonage = require('@vonage/server-sdk')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+
+
+app.get('/', function(req, res) {
+    const vonage = new Vonage({
+        apiKey: process.env.PORTAPI_KEY,
+        apiSecret: process.env.API_SECRET,
+        applicationId: process.env.APPLICATION_ID,
+        privateKey: process.env.PRIVATE_KEY
+    })
+    vonage.channel.send(
+        { "type": "mms", "number": req.query.msisdn},
+        { "type": "mms", "number": process.env.FROM_NUMBER},
+        {
+          "content": {
+            "type": "image",
+            "image": { "url": 'https://media.sproutsocial.com/uploads/meme-example.jpg' }
+          }
+        },
+        (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(data.message_uuid);
+            }
+        }
+    );
+    res.status(200).end();
+});
+
+app.listen(port, function() {
+});
